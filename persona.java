a3d1b6d88c30a2323e3bf9a7781b127adbb78223
Caleb <caleb.hunt789@gmail.com>
2026-06-17 17:17:49 +0000

    the start of the journey and the game plan

---
+++ persona.java
@@ -0,0 +1,7 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
